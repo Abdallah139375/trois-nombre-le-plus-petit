ac15f8270343d000a084eb109393206c3eda7cbd
hocin <hocin@LAPTOP-MS88BQ4T>
2024-10-30 15:24:21 +0100

    Ajoutez des fichiers projet.

---
+++ trois nombre le plus petit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trois_nombre_le_plus_petit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declarations 
+            double nb1,nb2 ,nb3;
+
+            //saisie de trois nombre 
+
+            Console.WriteLine("entrez un premier nombre = ");
+            nb1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("entrez un deuxieme nombre = ");
+            nb2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("entrez un troisiéme nombre = ");
+            nb3 = double.Parse(Console.ReadLine());
+
+            //afficher le plus petit des trois nombres 
+            Console.WriteLine("Lle plus petit des trois est " + Math.Min(nb1, Math.Min(nb2, nb3)));
+            Console.ReadLine();
+
+        }
+        
+
+    }
+}
